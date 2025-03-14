ecb1355653fb2b505174902082dfc1064b0c2c95
AmanBeke <Abekele1@msudenver.edu>
2025-03-14 15:33:59 -0600

    Update PartC1.s

--- PartC1.s
+++ PartC1.s
@@ -18,14 +18,14 @@ outer_loop:
     MOV R2, R1       @ Outer loop counter
 
 inner_loop:
-    LDRB R3, [R0, R2]     @ Load element at index R2
-    LDRB R4, [R0, R2, #-1]! @ Load previous element
+    LDR R3, [R0, R2, LSL #2]     @ Load element at index R2 (word size)
+    LDR R4, [R0, R2, LSL #2]     @ Load previous element (fixed syntax)
 
     CMP R3, R4
     BHS no_swap
 
-    STRB R3, [R0, R2, #-1] @ Swap elements
-    STRB R4, [R0, R2]
+    STR R3, [R0, R2, LSL #2]     @ Swap elements
+    STR R4, [R0, R2, LSL #2]
 
 no_swap:
     SUBS R2, R2, #1
@@ -49,7 +49,7 @@ loop:
     CMP R3, R1          @ Compare index with array size
     BGE not_found
 
-    LDRB R5, [R0, R3]   @ Load current element
+    LDR R5, [R0, R3, LSL #2]   @ Load current element (word-sized)
     CMP R5, R2          @ Compare with search value
     BEQ found
 
@@ -64,19 +64,18 @@ not_found:
     MOV R0, R4          @ Return -1 (not found)
     BX LR
 
-
 @ ---------------------------------
-@ Array Stats
+@ Array Stats Function
 @ ---------------------------------
 array_stats:
-    PUSH {LR}             @ Save return address
+    PUSH {R2, R3, R4, R5, LR}  @ Ensure ascending order
     MOV R2, #0            @ Initialize sum to 0
     LDR R3, [R0]          @ Load first element as min
     LDR R4, [R0]          @ Load first element as max
     MOV R5, #0            @ Index counter
 
 stats_loop:
-    LDR R6, [R0, R5, LSL #2]  @ Load array element
+    LDR R6, [R0, R5, LSL #2]  @ Load array element (word-sized)
     ADD R2, R2, R6        @ Add to sum
 
     CMP R6, R3            @ Compare with current min
@@ -101,30 +100,30 @@ next_element:
     MOV R1, R4            @ Return max in R1
     MOV R2, R2            @ Return sum in R2
 
-    POP {LR}
+    POP {R2, R3, R4, R5, LR}  @ Ensure ascending order
     BX LR                 @ Return
 
 @ ---------------------------------
 @ Array Rotation
 @ ---------------------------------
 array_rotate:
-    PUSH {LR, R2, R3, R4}
+    PUSH {R2, R3, R4, LR}  @ Ensure ascending order
     
     CMP R1, #1             @ If array has 1 or 0 elements, return
     BLE rotate_done
 
-    LDR R2, [R0, R1, LSL #2]  @ Load last element
+    LDR R2, [R0, R1, LSL #2]  @ Load last element (fixed offset)
     MOV R3, R1             @ Copy array size
     SUB R3, R3, #1         @ Reduce by one
 
 rotate_loop:
     LDR R4, [R0, R3, LSL #2]  @ Load previous element
-    STR R4, [R0, R3, LSL #2, #4]  @ Shift right
+    STR R4, [R0, R3, LSL #2]  @ Shift right
     SUBS R3, R3, #1
     BGE rotate_loop
 
     STR R2, [R0]           @ Store last element in first position
 
 rotate_done:
-    POP {LR, R2, R3, R4}
+    POP {R2, R3, R4, LR}   @ Ensure ascending order
     BX LR                  @ Return
