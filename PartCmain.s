7149a63d79b452a5c76985342716c2c959cd6154
AmanBeke <Abekele1@msudenver.edu>
2025-03-14 15:46:58 -0600

    Update PartCmain.s

--- PartCmain.s
+++ PartCmain.s
@@ -1,26 +1,31 @@
     .section .data
 array:
-    .byte 10, 3, 8, 1, 6, 4, 2, 7, 5
+    .word 10, 3, 8, 1, 6, 4, 2, 7, 5   @ Example array
 array_size:
-    .word 9
-
+    .word 9                            @ Size of the array
 search_value:
-    .word 6
+    .word 6                            @ Value to search
 
     .section .text
     .global _start
     .extern sort
     .extern array_search
+    .extern array_stats
+    .extern array_rotate
 
 _start:
-    LDR R0, =array    @ Load array address
+    LDR R0, =array        @ Load array address
     LDR R1, =array_size
-    LDR R1, [R1]      @ Load array size
-    BL sort           @ Call sorting function
+    LDR R1, [R1]          @ Load array size
+    BL sort               @ Call sorting function
 
     LDR R2, =search_value
-    LDR R2, [R2]      @ Load search value
-    BL array_search   @ Call search function
+    LDR R2, [R2]          @ Load search value
+    BL array_search       @ Call search function
+
+    BL array_stats        @ Compute min, max, sum
+
+    BL array_rotate       @ Rotate array right by 1 position
 
     @ Exit program
     MOV R7, #1
